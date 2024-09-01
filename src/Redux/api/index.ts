c1e3688fd5c1785ac130a158b62cefbcdec7dea1
Vladislav <102241947+vladislavslutskii@users.noreply.github.com>
2024-09-01 15:04:31 +0300

    Update index.ts

--- src/Redux/api/index.ts
+++ src/Redux/api/index.ts
@@ -2,7 +2,7 @@ import { create } from "apisauce";
 import { PER_PAGE } from "../../Utils";
 
 const API = create({
-  baseURL: "https://studapi.teachmeskills.by/",
+  baseURL: "https://studapi.teachmeskills.by",
 });
 
 const getPostsList = (
